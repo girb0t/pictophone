41bda525599fe775dd0cf16601918544ea8bc37e
Pei Xiong Liu <peixiong.liu@gmail.com>
2017-01-20 00:28:13 -0500

    Import Button component

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+// naming this file 'index.js' will allow React to automagically look for this file when
+// declaring: `import { widget1, widget2 } from "./components/common"`
+
+// `export * from ...` syntax is used to re-export all exports of the imported module.
+
+export * from './Button';
